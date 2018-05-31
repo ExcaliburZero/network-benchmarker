58b4d9c50419dee8c1dd2e220b730241ab7a3650
Christopher Wells <cwellsny@nycap.rr.com>
2018-05-31 00:12:53 -0400

    Start working on data fetching

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ const express = require("express");
 const app = express();
 
 const PORT = 5023;
-const DB_NAME = "NetworkBenchmarker";
 
-app.get("/", (req,res) => res.send("Hello, World!"));
+const DB_NAME = "NetworkBenchmarker";
+const RESULTS_TABLE = "Results";
+
+const DB_URL = "mongodb://localhost/";
+
+const MongoClient = require("mongodb").MongoClient;
 
 function start() {
     console.log("Running server on port: " + PORT);
 
-    var MongoClient = require("mongodb").MongoClient;
-    var url = "mongodb://localhost/" + DB_NAME;
+    MongoClient.connect(DB_URL, function (err, db) {
+        const dbo = db.db(DB_NAME);
 
-    MongoClient.connect(url, function (err, db) {
         console.log("Connected");
         db.close();
     });
 }
 
+function getData(req,res) {
+    MongoClient.connect(DB_URL, function(err, db) {
+        const dbo = db.db(DB_NAME);
+
+        const cursor = dbo.collection(RESULTS_TABLE).find();
+
+        cursor.each(function (err, doc) {
+            console.log(doc);
+        });
+
+        res.send("Data");
+        db.close();
+    });
+}
+
+app.get("/", (req,res) => res.send("Hello, World!"));
+app.get("/data", getData);
 app.listen(PORT, start);
