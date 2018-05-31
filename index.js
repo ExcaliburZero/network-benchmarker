521220adfde7fbc1eea24271942dafee4ebf6363
Christopher Wells <cwellsny@nycap.rr.com>
2018-05-30 22:13:12 -0400

    Start working with MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express")
 const app = express()
 
-const port = 5023
+const PORT = 5023
+const DB_NAME = "NetworkBenchmarker"
 
 app.get("/", (req,res) => res.send("Hello, World!"))
 
-app.listen(port, () => console.log("Running server on port: " + port))
+function start() {
+    console.log("Running server on port: " + PORT)
+
+    var MongoClient = require("mongodb").MongoClient;
+    var url = "mongodb://localhost/" + DB_NAME
+
+    MongoClient.connect(url, function (err, db) {
+        console.log("Connected");
+        db.close();
+    });
+}
+
+app.listen(PORT, start)
