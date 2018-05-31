3195896e70b37dcce286350d133dca5971c7ccef
Christopher Wells <cwellsny@nycap.rr.com>
2018-05-31 01:31:27 -0400

    Implement request benchmarking

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require("request-promise");
+
 const app = express();
 
 const PORT = 5023;
@@ -10,8 +12,52 @@ const DB_URL = "mongodb://localhost/";
 
 const MongoClient = require("mongodb").MongoClient;
 
+const REQUEST_URL = "http://www.google.com";
+
+const NUM_REQUESTS = 10;
+
+async function sendRequest() {
+    await request.get(REQUEST_URL);
+
+    return true;
+}
+
 function getCurrentDatetime() {
     return new Date();
+}
+
+function getTimeNano() {
+    return process.hrtime()[1];
+}
+
+function timeRequest() {
+    const before = getTimeNano();
+
+    const sucessful = sendRequest();
+
+    const after = getTimeNano();
+    const difference = after - before;
+
+    return difference;
+}
+
+function benchmarkMeanLatency() {
+    // Ignore the first request
+    timeRequest();
+
+    var latencySum = 0.0;
+    for (i = 0; i < NUM_REQUESTS; i++) {
+        const elapsed = timeRequest();
+
+        // Divide by 2 to account for both way travel
+        const latency = elapsed / 2.0;
+
+        latencySum += latency;
+    }
+
+    const meanLatency = latencySum / NUM_REQUESTS;
+
+    return meanLatency;
 }
 
 function start() {
@@ -20,11 +66,13 @@ function start() {
     MongoClient.connect(DB_URL, function (err, db) {
         if (err) throw err;
 
+        const meanLatency = benchmarkMeanLatency();
+
         const dbo = db.db(DB_NAME);
 
         const result = {
             datetime: getCurrentDatetime(),
-            mean_latency: 5.0
+            mean_latency: meanLatency
         };
 
         dbo.collection(RESULTS_TABLE).insertOne(result, function(err, res) {
