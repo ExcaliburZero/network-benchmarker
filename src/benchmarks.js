2aa97ad3123e3a2027857a1fd2a086847505b4e1
Christopher Wells <cwellsny@nycap.rr.com>
2018-06-01 08:01:32 -0400

    Start documenting benchmarks module

--- src/benchmarks.js
+++ src/benchmarks.js
@@ -7,16 +7,43 @@ const MEASUREMENT_SAMPLES = 10;
 
 const STDDEV_FILTER_RATIO = 2.0; // Allow <= ratio * std deviation from mean
 
+/**
+ * Sends a synchronus request to the request url in order to allow for latency
+ * to be benchmarked.
+ */
 async function sendRequest() {
     await request.get(REQUEST_URL);
 
     return true;
 }
 
+/**
+ * Returns the current time in nanoseconds. This is used to calculate the
+ * amount of time that a web request takes.
+ *
+ * It is important to have accuracy in the range of nanoseconds, as opposed to
+ * miliseconds, in order to get a good measure of the web request durations,
+ * which can be quite small.
+ *
+ * @example
+ * getTimeNano()
+ * // 80661361
+ */
 function getTimeNano() {
     return process.hrtime()[1];
 }
 
+/**
+ * Makes a synchronus web request and returns the time that it took to get a
+ * response (in nanoseconds).
+ *
+ * Note that function uses a web request, so it is non-deterministic, performs
+ * IO, and requires an internet connection in order to work properly.
+ *
+ * @example
+ * timeRequest()
+ * // 13330469
+ */
 function timeRequest() {
     const before = getTimeNano();
 
@@ -28,6 +55,12 @@ function timeRequest() {
     return difference;
 }
 
+/**
+ * Calculate statistics on the given set of benchmark latency measurements.
+ *
+ * Calculates the mean and standard deviation, before and after filtering out
+ * some outliers. Also records how many outliers are filtered out.
+ */
 function calculateStatistics(latencies) {
     const meanLatency = stats.mean(latencies);
     const stdLatency = stats.standardDeviation(latencies, meanLatency);
@@ -48,8 +81,25 @@ function calculateStatistics(latencies) {
     };
 }
 
+/**
+ * Runs a benchmark of the network and returns statistics from the results.
+ *
+ * Currently only examines network latency.
+ *
+ * Note that since this function runs a benchmark using a web request, it is
+ * non-deterministic, performs IO, and requires an internet connection in order
+ * to work properly.
+ *
+ * @example
+ * benchmark()
+ * // { meanLatencyBefore: 97799.85,
+ * //   stdLatencyBefore: 53879.88518735447,
+ * //   meanLatencyAfter: 82047.83333333333,
+ * //   stdLatencyAfter: 27282.63114958754,
+ * //   numDroppedPoints: 1 }
+ */
 function benchmark() {
-    // Ignore the first request
+    // Ignore the first request, as it is often an outlier
     timeRequest();
 
     var latencySum = 0.0;
